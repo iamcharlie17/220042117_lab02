4de6f2d18786ff4957ec6708c8bfd3417be839c0
Riyad Hosen <riyadhosen@Riyads-MacBook-Air.local>
2024-09-23 10:15:40 +0600

    add: test cases for task 1

--- src/test/FizzyPrinterTest.java
+++ src/test/FizzyPrinterTest.java
@@ -12,5 +12,34 @@ public class FizzyPrinterTest {
         FizzyPrinter fizzyPrinter = new FizzyPrinter();
         assertEquals("Fizz", fizzyPrinter.printFizzy(9, false));
     }
+    @Test
+    public void testDivisibleBy5() {
+        FizzyPrinter fizzyPrinter = new FizzyPrinter();
+        assertEquals( "Buzz", fizzyPrinter.printFizzy(10, false));
+    }
+
+    @Test
+    public void testDivisibleBy7() {
+        FizzyPrinter fizzyPrinter = new FizzyPrinter();
+        assertEquals( "Bang", fizzyPrinter.printFizzy(14, false));
+    }
+    @Test
+    public void testDivisibleBy3And5() {
+        FizzyPrinter fizzyPrinter = new FizzyPrinter();
+        assertEquals("Should return 'FizzBuzz' when divisible by 3 and 5", "FizzBuzz", fizzyPrinter.printFizzy(15, false));
+    }
+
+    @Test
+    public void testDivisibleBy3And5And7() {
+        FizzyPrinter fizzyPrinter = new FizzyPrinter();
+        assertEquals("Should return 'FIZZBUZZBANG' when divisible by 3, 5, and 7 in uppercase", "FIZZBUZZBANG", fizzyPrinter.printFizzy(105, true));
+    }
+
+    @Test
+    public void testNotDivisibleBy3Or5Or7() {
+        FizzyPrinter fizzyPrinter = new FizzyPrinter();
+        assertEquals("Should return 'Boom' when not divisible by 3, 5, or 7", "Boom", fizzyPrinter.printFizzy(8, false));
+    }
+    
 
 }
